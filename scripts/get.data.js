5ddcb271c8aca9ccdac60900b074afdfe88058d8
Nikita <nikita.chefonov@mail.ru>
2019-05-15 13:29:30 +0700

    Promise

--- scripts/get.data.js
+++ scripts/get.data.js
@@ -3,27 +3,40 @@ var data;
 var student;
 
 $("#getData_button").click(() => {
-    data = {
-        username: $("#username").val(),
-        password: $("#password").val(),
-        appToken: $("#token").val()
-    };
+    return new Promise((resolve, reject) => {
+        data = {
+            username: $("#username").val(),
+            password: $("#password").val(),
+            appToken: $("#token").val()
+        };
 
-    console.log("Send data");
-    console.log(data.username);
-
-    $.ajax({
-      type: "POST",
-      url: "http://193.218.136.174:8080/cabinet/rest/auth/login",
-      data: JSON.stringify(data),
-      success: function(data){
-          student = data;
-          console.log(student);
-      },
-
-      error: function(error){
-            console.log(error);
-      }
-
+        // Авторизация
+        $.ajax({
+          type: "POST",
+          url: "http://193.218.136.174:8080/cabinet/rest/auth/login",
+          data: JSON.stringify(data),
+          success: function(data){
+              student = data;
+              console.log(student)
+              student.token = student.usertoken;
+              resolve();
+          },
+          contentType: "application/json"
+        });
+    }).then(() => {
+        // Базовая информация
+        $.ajax({
+          type: "POST",
+          url: "http://193.218.136.174:8080/cabinet/rest/student/get",
+          data: JSON.stringify({
+              text: "",
+              userToken: student.token
+          }),
+          success: function(data){
+              student = data;
+              console.log(student);
+          },
+          contentType: "application/json",
+        });
     });
 });
