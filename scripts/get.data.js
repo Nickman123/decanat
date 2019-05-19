6c015504dbd94f7878ae0ba239822909d3aabe19
nikita <nikita.chefonov@mail.ru>
2019-05-20 00:08:39 +0700

    Authorization + initial info

--- scripts/get.data.js
+++ scripts/get.data.js
@@ -1,42 +1,56 @@
-// Информация для получения данных от сервера
-var data;
-var student;
+// Пользователь системы
+var student = {
+    token: ''
+};
 
 $("#getData_button").click(() => {
     return new Promise((resolve, reject) => {
-        data = {
-            username: $("#username").val(),
-            password: $("#password").val(),
-            appToken: $("#token").val()
-        };
-
-        // Авторизация
-        $.ajax({
-          type: "POST",
-          url: "http://193.218.136.174:8080/cabinet/rest/auth/login",
-          data: JSON.stringify(data),
-          success: function(data){
-              student = data;
-              console.log(student)
-              student.token = student.usertoken;
-              resolve();
-          },
-          contentType: "application/json"
-        });
+        authorizeUser(resolve);
     }).then(() => {
-        // Базовая информация
-        $.ajax({
-          type: "POST",
-          url: "http://193.218.136.174:8080/cabinet/rest/student/get",
-          data: JSON.stringify({
-              text: "",
-              userToken: student.token
-          }),
-          success: function(data){
-              student = data;
-              console.log(student);
-          },
-          contentType: "application/json",
-        });
+        getInitialData();
     });
 });
+
+
+/**
+ * Авторизация поьзователя в системе и получение токена
+ */
+var authorizeUser = function(resolve) {
+   var data = {
+        username: $("#username").val(),
+        password: $("#password").val(),
+        appToken: $("#token").val()
+    };
+
+    // Авторизация
+    $.ajax({
+      type: "POST",
+      url: "http://193.218.136.174:8080/cabinet/rest/auth/login",
+      data: JSON.stringify(data),
+      success: function(data){
+          data = JSON.parse(data);
+          student.token = data.usertoken;  // Токен пользователя
+          resolve()
+      },
+      contentType: "application/json"
+    });
+}
+
+/**
+ * Получение базовой информации о пользователе
+ */
+var getInitialData = function() {
+    $.ajax({
+      type: "POST",
+      url: "http://193.218.136.174:8080/cabinet/rest/student/get",
+      data: JSON.stringify({
+          text: "",
+          userToken: student.token
+      }),
+      success: function(data){
+          console.log(student.token);
+          console.log(data);
+      },
+      contentType: "application/json",
+    });
+}
